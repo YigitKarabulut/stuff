7b6355cdea87b305a642008e85e54feebbb57efc
yigit <yigitkarabulut51@gmail.com>
2022-09-20 23:21:58 +0300

    Stuff

--- stuff/Model/ElectronicModel.swift
+++ stuff/Model/ElectronicModel.swift
@@ -12,9 +12,8 @@ class ElectronicModel {
     static let sharedInstance = ElectronicModel()
     
     var category = "Electronic"
+    var electronicTool = ""
     var brand = ""
-    var model = ""
-    var status = ""
     var advertTitle = ""
     var explanation = ""
     var firsImage = UIImage()
@@ -25,6 +24,15 @@ class ElectronicModel {
     var locationLongitude = ""
     var electronicList = [String]()
     var laptopList = [String]()
+    var smartwatchList = [String]()
+    var gameconsolesList = [String]()
+    var tabletList = [String]()
+    var cameraList = [String]()
+    var phoneAccessoriesList = [String]()
+    var computerAccessoriesList = [String]()
+    var headphoneList = [String]()
+    var tvList = [String]()
+    var desktopComputerList = ["New", "Good", "Worn"]
     
     func getElectronicData(){
         let query = PFQuery(className: "Electronic")
@@ -35,7 +43,7 @@ class ElectronicModel {
                 if objects != nil {
                     if objects!.count > 0 {
                         for object in objects! {
-                            self.electronicList.removeAll(keepingCapacity: false)
+                            
                             if let name = object.object(forKey: "name") as? String {
                                 self.electronicList.append(name)
                             }
@@ -67,6 +75,171 @@ class ElectronicModel {
         }
 
     }
+    func getSmartWatchData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Smart Watch").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.smartwatchList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.smartwatchList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
+    func getGameConsolesData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Game Consoles").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.gameconsolesList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.gameconsolesList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    func getTabletData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Tablet").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.tabletList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.tabletList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
+    func getCameraData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Camera").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.cameraList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.cameraList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
+    func getPhoneAccessoriesData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Phone Accessories").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.phoneAccessoriesList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.phoneAccessoriesList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    func getComputerAccessoriesData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Computer Accessories").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.computerAccessoriesList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.computerAccessoriesList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    func getHeadphoneData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "Headphone").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.headphoneList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.headphoneList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    func getTvData(){
+        let query = PFQuery(className: "ElectronicTool")
+        query.whereKey("category", equalTo: "TV").findObjectsInBackground { objects, error in
+            if error != nil {
+                
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.tvList.removeAll(keepingCapacity: false)
+                        for object in objects! {
+                            if let name = object.object(forKey: "brand") as? String {
+                                self.tvList.append(name)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    
     
     
     
